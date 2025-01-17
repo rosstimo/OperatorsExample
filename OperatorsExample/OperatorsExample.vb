17c41f14559314e91fa6eb0b73d530962b2bd297
tim <rosstimo@isu.edu>
2025-01-17 08:19:36 -0700

    add more operator examples

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -7,8 +7,27 @@
 Module OperatorsExample
 
     Sub Main()
+
+        'sum operator
         Console.Write("5 + 3 = ") 'print a string
         Console.WriteLine(5 + 3) 'print the sum 
+        Console.WriteLine()
+
+        'difference operator
+        Console.Write("5 - 3 = ") 'print a string
+        Console.WriteLine(5 - 3) 'print the sum 
+        Console.WriteLine()
+
+        'multiplication operator
+        Console.Write("5 * 3 = ") 'print a string
+        Console.WriteLine(5 * 3) 'print the sum 
+        Console.WriteLine()
+
+        'floating point division operator
+        Console.Write("5 / 3 = ") 'print a string
+        Console.WriteLine(5 / 3) 'print the sum 
+        Console.WriteLine()
+
 
     End Sub
 
